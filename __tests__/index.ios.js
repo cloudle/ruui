9329b556fa8ff382c3aa1bb5f4c3e5b72113c192
cloud <lehaoson@gmail.com>
2017-02-08 15:30:29 +0700

    Change test entry, add Redux Store.

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,14 @@
 import 'react-native';
 import React from 'react';
-import Index from '../src/app';
+import Index from '../example';
+import configureStore from '../example/store';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const store = configureStore();
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <Index store={store}/>
   );
 });
