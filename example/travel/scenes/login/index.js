605bcd98031a464fcdbae3d9a0d1f8c1921b44cc
cloud <lehaoson@gmail.com>
2017-02-10 15:43:39 +0700

    Finish demo login page.

--- example/travel/scenes/login/index.js
+++ example/travel/scenes/login/index.js
@@ -22,9 +22,28 @@ class LoginScene extends Component {
         <Input floatingLabel={this.props.localize.placeholders.userAccount}
                ref="username" maxLength={128}/>
         <Input floatingLabel={this.props.localize.placeholders.password}
+               underLineStyle={{backgroundColor: '#d3721b'}}
                ref="password" password={true}/>
       </View>
 
+	    <Text style={styles.forgotPasswordText}>
+		    {this.props.localize.titles.forgotPassword}
+	    </Text>
+
+	    <View style={styles.commandWrapper}>
+		    <Button
+			    wrapperStyle={[styles.buttonWrapper, styles.registerButton]}
+			    title={this.props.localize.titles.register}/>
+		    <Button
+			    wrapperStyle={[styles.buttonWrapper, styles.loginButton]}
+			    textStyle={{color: '#444444', fontWeight: '500'}}
+			    rippleColor="#222222"
+			    title={this.props.localize.titles.login}/>
+	    </View>
+
+	    <Text style={styles.supportText}>
+		    {this.props.localize.titles.support}
+	    </Text>
     </ScrollView>
   }
 }
@@ -61,4 +80,32 @@ const styles = StyleSheet.create({
 		marginBottom: 10,
 		overflow: 'hidden',
 	},
+	commandWrapper: {
+		width: ScreenWidthPadding(40, 290),
+		flexDirection: 'row'
+	},
+	errorText: {
+		color: 'orange',
+	},
+	forgotPasswordText: {
+		color: 'white',
+		marginTop: 20,
+		marginBottom: 15,
+		textDecorationLine: 'underline',
+	},
+	supportText: {
+		color: 'white',
+		marginTop: 20,
+		fontSize: 12,
+	},
+	buttonWrapper: {
+		flex: 1,
+		marginLeft: 5, marginRight: 5,
+	},
+	registerButton: {
+		backgroundColor: "#9E9E9E",
+	},
+	loginButton: {
+		backgroundColor: "#FFFFFF",
+	}
 });
