56a21afdc53bde2a65165d49f967f755ecd6897c
cloud <lehaoson@gmail.com>
2017-02-18 04:33:05 +0700

    isTouchDevice helper for Web.

--- src/utils/screen.js
+++ src/utils/screen.js
@@ -6,3 +6,12 @@ export function ScreenWidthPadding (padding, maxSize): Number {
 	let paddedSize = ScreenSize.width - (padding * 2);
 	return paddedSize > maxSize ? maxSize : paddedSize;
 }
+
+export function isTouchDevice() {
+	try {
+		document.createEvent("TouchEvent");
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
