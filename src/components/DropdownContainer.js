f96ea39c92e9530e2be90da014ba9013b0c65de8
cloudle <lehaoson@gmail.com>
2017-08-22 18:02:33 +0700

    Support Dropdown toggle event props (when to trigger dropdown).

--- src/components/DropdownContainer.js
+++ src/components/DropdownContainer.js
@@ -7,7 +7,10 @@ import { Style, Element, SnappingDirection, Layout } from '../typeDefinition';
 
 type Props = {
 	dispatch?: Function,
+	dropdownEvent?: 'onPress' | 'onLongPress',
 	onPress?: Function,
+	onLongPress?: Function,
+	delayLongPress?: number,
 	style?: Style,
 	children?: Element,
 	containerLayout?: Layout,
@@ -29,6 +32,7 @@ export default class DropdownContainer extends Component {
 	props: Props;
 
 	static defaultProps = {
+		dropdownEvent: 'onPress',
 		dropdownDirection: 'bottom',
 		dropdownSpacing: 10,
 		offset: { top: 0, left: 0 },
@@ -36,7 +40,9 @@ export default class DropdownContainer extends Component {
 
 	render() {
 		return <TouchableOpacity
-			onPress={this.onPress}>
+			onPress={this.onPress}
+			onLongPress={this.onLongPress}
+			delayLongPress={this.props.delayLongPress}>
 			<View
 				style={this.props.style}
 				ref={(container) => { this.container = container; }}>
@@ -47,6 +53,15 @@ export default class DropdownContainer extends Component {
 
 	onPress = (e) => {
 		if (this.props.onPress) this.props.onPress(e);
+		if (this.props.dropdownEvent === 'onPress') this.toggleDropdown();
+	};
+
+	onLongPress = (e) => {
+		if (this.props.onLongPress) this.props.onLongPress(e);
+		if (this.props.dropdownEvent === 'onLongPress') this.toggleDropdown();
+	};
+
+	toggleDropdown = () => {
 		this.container.measure((a, b, width, height, px, py) => {
 			this.props.dispatch(appActions.toggleDropdown(true, {
 				wrapperStyle: this.props.dropdownWrapperStyle,
