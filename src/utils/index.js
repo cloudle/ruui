2b1c12176ab75636fe4056dd9ab48ccc75542c25
cloudle <lehaoson@gmail.com>
2017-09-26 16:17:43 +0700

    isServer utils (for SSR).

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,8 +4,9 @@ import tinyColorImport from 'tinycolor2';
 export const tinyColor = tinyColorImport;
 
 export const os = Platform.OS;
+export const isServer = !!(typeof module !== 'undefined' && module.exports);
 export const isIos = os === 'ios';
-export const isBrowser = os === 'web';
+export const isBrowser = !isServer && os === 'web';
 export const isAndroid = os === 'android';
 
 export * from './colors';
