d90d1aebb7944920baf718efd0a725f40be4c17a
cloudle <lehaoson@gmail.com>
2018-01-09 00:57:20 +0700

    More flexible on controlling EnterAnimation decorator..

--- src/decorators/EnterAnimation.js
+++ src/decorators/EnterAnimation.js
@@ -2,26 +2,25 @@ import React from 'react';
 import { Animated, Easing } from 'react-native';
 
 export default function ({
-		enterSpeed = 500,
-		delay = 0,
+		speed = 500, delay = 0, fromValue = 0, toValue = 1,
+		easing = Easing.out(Easing.cubic),
 	} = {}) {
 	return function (BaseComponent) {
 		return class EnterAnimationEnhancer extends BaseComponent {
 			constructor(props) {
 				super(props);
 				this.state = {
-					enterAnimation: new Animated.Value(1),
+					enterAnimation: new Animated.Value(toValue),
 				};
 			}
 
 			componentDidMount() {
-				this.state.enterAnimation.setValue(0);
+				this.state.enterAnimation.setValue(fromValue);
 
 				setTimeout(() => {
 					Animated.timing(this.state.enterAnimation, {
-						toValue: 1,
-						duration: enterSpeed,
-						easing: Easing.out(Easing.cubic),
+						toValue, easing,
+						duration: speed,
 					}).start(() => {
 						this.setState({ animationFinished: true });
 						super.animationDidFinish && super.animationDidFinish();
