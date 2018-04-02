56e9b2d91ce84077bdac15ead2c503a46ff9c0f3
cloudle <lehaoson@gmail.com>
2018-04-02 16:07:28 +0700

    Remove embedded Tooltip way (in ResponsibleTouchArea)..

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,122 +1,101 @@
 import React, { Component } from 'react';
-import { Dimensions, Animated, Easing, View, Text, StyleSheet } from 'react-native';
+import { Animated, StyleSheet, View, Text, Easing, } from 'react-native';
+import { isString } from 'lodash';
 
-import { isBrowser, directionSnap } from '../utils';
-import { Style, Element, SnappingDirection } from '../typeDefinition';
+import { connect, isBrowser, directionSnap, directionAnimatedConfigs } from '../utils';
+import type { Dimension, Layout, PositionOffset, Element, Style, } from '../typeDefinition';
 
 type Props = {
-	wrapperStyle?: Style,
-	direction?: SnappingDirection,
-	positionSpacing?: number,
-	positionOffset?: Object,
-	containerSize?: Object,
-	children?: string | Element,
+	tooltip?: {
+		active?: Boolean,
+		configs?: {
+			targetLayout: Layout,
+			direction: String,
+			positionSpacing?: Number,
+			positionOffset?: PositionOffset,
+			wrapperStyle?: Style,
+			content?: String | Element,
+		},
+	},
+	screenSize?: Dimension,
 };
 
-export default class Tooltip extends Component<any, Props, any> {
-	props: Props;
-
-	static defaultProps = {
-		direction: 'top',
-		positionSpacing: 10,
-		containerSize: { width: 0, height: 0 },
-		positionOffset: { top: 0, left: 0 },
+@connect(({ dimensions, tooltip }) => {
+	return {
+		screenSize: dimensions.window,
+		tooltip,
 	};
+})
+
+export default class RuuiTooltip extends Component {
+	props: Props;
 
 	constructor(props) {
 		super(props);
+		this.enterAnimation = new Animated.Value(0);
 		this.state = {
-			enterAnimation: new Animated.Value(0),
+			top: 0, left: 0,
 		};
 	}
 
-	componentDidMount() {
-		const ScreenSize = Dimensions.get('window');
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.tooltip.active !== this.props.tooltip.active) {
+			this.container.measure((x, y, width, height) => {
+				const { targetLayout, direction, positionSpacing } = this.props.tooltip.configs;
 
-		this.container.measure((a, b, width, height, px, py) => {
-			const containerSize = this.props.containerSize || { width: 0, height: 0 },
-				snappingPosition = directionSnap(
-					0, 0, containerSize.width, containerSize.height, width, height,
-					this.props.direction, this.props.positionSpacing),
-				rightEdgeDistance = (ScreenSize.width - 5) - (px + snappingPosition.left + width),
-				leftEdgeDistance = px + snappingPosition.left,
-				bottomEdgeDistance = (ScreenSize.height - 5) - (py + snappingPosition.top + height),
-				topEdgeDistance = py + snappingPosition.top;
+				this.setState(directionSnap(
+					targetLayout.y, targetLayout.x,
+					targetLayout.width, targetLayout.height,
+					width, height,
+					direction, positionSpacing));
 
-			let topOffset = snappingPosition.top + this.props.positionOffset.top,
-				leftOffset = snappingPosition.left + this.props.positionOffset.left;
-
-			if (leftEdgeDistance < 0) {
-				leftOffset = leftEdgeDistance + 5;
-			} else if (rightEdgeDistance < 0) {
-				leftOffset = rightEdgeDistance;
-			}
-
-			if (topEdgeDistance < 0) {
-				topOffset = topEdgeDistance + 5;
-			} else if (bottomEdgeDistance < 0) {
-				topOffset = bottomEdgeDistance;
-			}
-
-			this.setState({ leftOffset, topOffset, });
-		});
-
-		Animated.timing(this.state.enterAnimation, {
-			toValue: 1,
-			duration: 500,
-			easing: Easing.in(Easing.bezier(0.23, 1, 0.32, 1)),
-		}).start();
+				Animated.timing(this.enterAnimation, {
+					toValue: nextProps.tooltip.active ? 1 : 0,
+					duration: 500,
+					easing: Easing.in(Easing.bezier(0.23, 1, 0.32, 1)),
+				}).start();
+			});
+		}
 	}
 
 	render() {
-		const children = typeof this.props.children === 'string'
-				? <Text numberOfLines={1} style={styles.title}>{this.props.children}</Text>
-				: this.props.children,
-			translate = this.state.enterAnimation.interpolate({
-				inputRange: [0, 1], outputRange: [15, 0],
-			}), borderRadius = this.state.enterAnimation.interpolate({
-				inputRange: [0, 0.5, 1], outputRange: [50, 15, 3],
-			}), opacity = this.state.enterAnimation.interpolate({
-				inputRange: [0, 1], outputRange: [0.2, 1],
-				extrapolate: 'clamp',
-			}),
-			containerStyles = {
-				transform: [{ translateY: translate }],
+		const animatedConfigs = directionAnimatedConfigs(
+				this.props.tooltip.configs.direction, 10, this.enterAnimation),
+			wrapperStyle = {
+				top: this.state.top, left: this.state.left,
+				transform: animatedConfigs.transform,
+				opacity: animatedConfigs.opacity,
 			},
-			innerStyles = {
-				borderTopLeftRadius: borderRadius,
-				borderTopRightRadius: borderRadius,
-				opacity,
-			},
-			edgeOffsets = {
-				left: this.state.leftOffset,
-				top: this.state.topOffset,
-			};
+			innerStyles = animatedConfigs.borderRadius;
 
-		return <Animated.View
-			pointerEvents="box-none"
-			style={[styles.container, containerStyles]}>
-			<View style={edgeOffsets} ref={(container) => { this.container = container; }}>
-				<Animated.View style={[styles.innerContainer, innerStyles, this.props.wrapperStyle]}>
-					{children}
+		return <Animated.View pointerEvents="none" style={[styles.wrapper, wrapperStyle]}>
+			<View ref={(instance) => { this.container = instance; }}>
+				<Animated.View style={[styles.container, innerStyles]}>
+					{this.renderContent()}
 				</Animated.View>
 			</View>
 		</Animated.View>;
 	}
+
+	renderContent = () => {
+		const content = this.props.tooltip.configs.content;
+
+		if (isString(content)) {
+			return <Text style={styles.title}>{content}</Text>;
+		} else {
+			return content;
+		}
+	};
 }
 
-const styles = StyleSheet.create({
-	container: {
-		zIndex: 12,
+export const styles = StyleSheet.create({
+	wrapper: {
 		position: isBrowser ? 'fixed' : 'absolute',
 	},
-	innerContainer: {
+	container: {
 		padding: 5, paddingHorizontal: 8,
 		backgroundColor: 'rgb(97, 97, 97)',
 		borderRadius: 3,
-	},
-	contentContainer: {
-		position: 'absolute',
 	},
 	title: {
 		fontSize: 11, fontWeight: '300',
