a4cb2b1df81131c1e3d5d5cfa798a02c07baa155
cloudle <lehaoson@gmail.com>
2018-04-02 11:15:36 +0700

    RuuiProvider watch for Dimensions and NetInfo..

--- src/components/ruuiProvider.js
+++ src/components/ruuiProvider.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { NetInfo, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import { merge } from 'lodash';
 
 import store from '../store';
+import { isServer } from '../utils';
 import coreConfigs from '../configs/core';
+import * as appActions from '../store/action/app';
 import { Element, RuuiConfigs } from '../typeDefinition';
 
 type Props = {
@@ -12,7 +15,7 @@ type Props = {
 	configs?: RuuiConfigs,
 };
 
-export default class RuuiContextProvider extends Component {
+export default class RuuiProvider extends Component {
 	static props: Props;
 	static defaultProps = {
 		store,
@@ -36,7 +39,57 @@ export default class RuuiContextProvider extends Component {
 		};
 	}
 
+	componentWillMount() {
+		if (!isServer && this.props.store) {
+			this.subscribeAndUpdateDimensions();
+			this.subscribeAndUpdateNetworkInfo();
+		}
+	}
+
+	componentWillUnmount() {
+		if (!isServer && this.props.store) {
+			NetInfo.removeEventListener(
+				'connectionChange', this.handleConnectionTypeChange);
+			NetInfo.isConnected.removeEventListener(
+				'connectionChange', this.handleIsConnectedChange);
+		}
+	}
+
 	render() {
 		return this.props.children;
 	}
+
+	subscribeAndUpdateDimensions = () => {
+		this.handleDimensionChange({
+			window: Dimensions.get('window'),
+			screen: Dimensions.get('screen'),
+		});
+
+		Dimensions.addEventListener('change', this.handleDimensionChange);
+	};
+
+	handleDimensionChange = (data) => {
+		this.store.dispatch(appActions.updateDimensionsInfo(data));
+	};
+
+	subscribeAndUpdateNetworkInfo = () => {
+		NetInfo.getConnectionInfo && NetInfo.getConnectionInfo()
+			.then(connectionInfo => this.handleConnectionTypeChange(connectionInfo));
+
+		NetInfo.isConnected.getConnectionInfo && NetInfo.isConnected.getConnectionInfo()
+			.then(isConnected => this.handleIsConnectedChange(isConnected));
+
+		NetInfo.addEventListener(
+			'connectionChange', this.handleConnectionTypeChange);
+		NetInfo.isConnected.addEventListener(
+			'connectionChange', this.handleIsConnectedChange);
+	};
+
+	handleConnectionTypeChange = (connectionType) => {
+		this.store.dispatch(appActions.updateNetInfo({ connectionType, }));
+	};
+
+	handleIsConnectedChange = (isConnected) => {
+		this.props.store.dispatch(appActions.updateNetInfo({ isConnected, }));
+	};
 }
