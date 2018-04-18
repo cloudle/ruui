43d1bc8ea42f326c970c6ac1bffa3bcdc1626016
cloudle <lehaoson@gmail.com>
2018-04-18 16:27:05 +0700

    automatically build cache if it not exists..

--- cli-local/commands/web.js
+++ cli-local/commands/web.js
@@ -1,13 +1,34 @@
+const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
+const webpack = require('webpack');
 const chalk = require('chalk');
 
 const port = process.env.ENV || 3000;
 
 function run() {
-	const root = process.cwd();
+	const cachePath = path.resolve(process.cwd(), 'web', 'vendor-manifest.json');
 
-	console.log('Preparing super awesome dev-server at', chalk.whiteBright(`localhost:${port}`), ':p');
+	if (fs.existsSync(cachePath)) {
+		runServer();
+	} else {
+		console.log(chalk.whiteBright('Building common chunk cache, this may take a while...'));
+		console.log(chalk.gray("(caching build will only run once on project's first time run)\n"));
+
+		setTimeout(() => {
+			const configs = require('../tools/webpack.vendor'),
+				compiler = webpack(configs);
+
+			compiler.run((error, stats) => {
+				if (error) console.log(error);
+				runServer('\n');
+			});
+		}, 0);
+	}
+}
+
+function runServer(prefix = '') {
+	console.log(`${prefix}Preparing super awesome dev-server at`, chalk.whiteBright(`localhost:${port}`), ':p');
 
 	setTimeout(() => {
 		const devServer = require('../tools/webpack.devserver');
