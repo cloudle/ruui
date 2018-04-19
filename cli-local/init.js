92d67d7e60e00a0540026467ad0021165cc9a651
cloudle <lehaoson@gmail.com>
2018-04-19 23:56:21 +0700

    Read and install dependencies, devDependencies from Template (including core-template - it's now use template dependencies instead hard-code).

--- cli-local/init.js
+++ cli-local/init.js
@@ -1,33 +1,69 @@
+const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const templateExclustions = ['dependencies.json', 'devDependencies.json', ];
+const rnCliModule = ext => path.resolve(process.cwd(), 'node_modules', 'react-native', 'local-cli', ext);
+const ruuiCliModule = ext => path.resolve(process.cwd(), 'node_modules', 'react-universal-ui', 'cli-local', ext);
+const isDirectory = source => fs.lstatSync(source).isDirectory();
 
-function rnCliModule(ext) {
-	return path.resolve(process.cwd(), 'node_modules', 'react-native', 'local-cli', ext);
-}
+const getDirectories = source => fs.readdirSync(source)
+	.map(name => path.join(source, name))
+	.filter(isDirectory);
 
-function ruuiCliModule(ext) {
-	return path.resolve(process.cwd(), 'node_modules', 'react-universal-ui', 'cli-local', ext);
-}
-
-function init(root, argsOrName) {
-	const walk = require(rnCliModule('util/walk.js')),
-		copyAndReplace = require(rnCliModule('util/copyAndReplace.js')),
+function init(root, argsOrName, opts) {
+	const template = opts['ruui-template'],
+		availableTemplates = getDirectories(path.resolve(ruuiCliModule('templates')))
+			.map(source => path.relative(ruuiCliModule('templates'), source))
+			.filter(name => name !== 'core'),
+		walk = require(rnCliModule('util/walk')),
+		copyAndReplace = require(rnCliModule('util/copyAndReplace')),
+		yarn = require(rnCliModule('util/yarn')),
+		yarnVersion = (!opts.npm) &&
+			yarn.getYarnVersionIfAvailable() &&
+			yarn.isGlobalCliUsingYarn(root),
+		templates = require('./util/templates'),
 		args = Array.isArray(argsOrName)
 			? argsOrName // argsOrName was e.g. ['AwesomeApp', '--verbose']
-			: [argsOrName].concat(process.argv.slice(4)), // argsOrName was e.g. 'AwesomeApp'
-		newProjectName = args[0];
-
-	const srcPath = path.resolve(ruuiCliModule('templates/core'));
-	walk(srcPath).forEach((absoluteSrcPath) => {
-		const relativeFilePath = path.relative(srcPath, absoluteSrcPath),
-			relativeRenamedPath = dotFilePath(relativeFilePath),
-			absoluteDestinationPath = path.resolve(root, relativeRenamedPath);
-
-		copyAndReplace(absoluteSrcPath, absoluteDestinationPath, {
+			: [argsOrName].concat(process.argv.slice(4)), // argsOrName was e.g. 'AwesomeApp';
+		newProjectName = args[0],
+		templateReplacements = {
 			'Hello App Display Name': newProjectName,
 			HelloWorld: newProjectName,
 			helloworld: newProjectName.toLowerCase(),
-		});
+		};
+
+	const coreTemplatePath = path.resolve(ruuiCliModule('templates/core'));
+	walk(coreTemplatePath).forEach((absoluteSrcPath) => {
+		const relativeFilePath = path.relative(coreTemplatePath, absoluteSrcPath),
+			relativeRenamedPath = dotFilePath(relativeFilePath),
+			absoluteDestinationPath = path.resolve(root, relativeRenamedPath);
+
+		if (templateExclustions.indexOf(relativeRenamedPath) >= 0) return;
+		copyAndReplace(absoluteSrcPath, absoluteDestinationPath, templateReplacements);
 	});
+
+	templates.installTemplateDependencies(coreTemplatePath, yarnVersion);
+	templates.installTemplateDevDependencies(coreTemplatePath, yarnVersion);
+
+	if (template) {
+		if (availableTemplates.indexOf(template) >= 0) {
+			const childTemplatePath = path.resolve(ruuiCliModule(`templates/${template}`));
+
+			walk(childTemplatePath).forEach((absoluteSrcPath) => {
+				const relativeFilePath = path.relative(childTemplatePath, absoluteSrcPath),
+					relativeRenamedPath = dotFilePath(relativeFilePath),
+					absoluteDestinationPath = path.resolve(root, relativeRenamedPath);
+
+				if (templateExclustions.indexOf(relativeRenamedPath) >= 0) return;
+				copyAndReplace(absoluteSrcPath, absoluteDestinationPath, templateReplacements);
+			});
+
+			templates.installTemplateDependencies(childTemplatePath, yarnVersion);
+			templates.installTemplateDevDependencies(childTemplatePath, yarnVersion);
+		} else {
+			console.log(chalk.yellow(`Couldn't found template with name "${template}". The project currently use default (essential) template!`));
+		}
+	}
 }
 
 function dotFilePath(filePath) {
