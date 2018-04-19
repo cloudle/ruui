efd7c709286202e40c46b1a75c965e7e6529070d
cloudle <lehaoson@gmail.com>
2018-04-19 23:50:37 +0700

    Allow init command, but warn the user that they're already in a React Universal project..

--- cli-local/commands/index.js
+++ cli-local/commands/index.js
@@ -1,5 +1,21 @@
+const path = require('path');
+const init = require('../init');
+
 module.exports = [
 	require('./web'),
 	require('./cache'),
 	require('./bundle'),
+	{
+		name: 'init',
+		func: () => {
+			console.log([
+				"Looks like we're already in a React Universal UI project folder",
+				'We should run this command from a different folder instead..',
+			].join('\n'));
+			// init(path.resolve('./'), 'SuperAwesomeProject', {
+			// 	_: ['init', 'SuperAwesomeProject'],
+			// 	'ruui-template': 'electron',
+			// });
+		},
+	},
 ];
