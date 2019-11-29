30574c6c610c1087c922c3ad24a6fd2eb63e6b8e
Cloud Le <lehaoson@gmail.com>
2019-11-30 03:09:45 +0700

    ruui.config.js template

---
+++ ruui.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	dev: (configs) => { /* <- customize devServer */
+		return configs;
+	},
+	webpack: (configs) => { /* <- customize webpack configures */
+		return configs;
+	}
+};
